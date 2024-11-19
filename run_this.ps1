d795e69210905514ef96ba57bb86e798efe09dc5
Luis-Guillermo-Rivera-Stephens <148508811+Luis-Guillermo-Rivera-Stephens@users.noreply.github.com>
2024-11-18 19:38:15 -0600

    init

---
+++ run_this.ps1
@@ -0,0 +1,14 @@
+Write-Host "Solo debes correr este script 1 vez"
+$nombre = Read-Host "Escribe tu nombre"
+echo "$nombre cloned this repository at $(Get-Date)" >> clone_logs.txt
+
+$flag = Read-Host "Quieres crear un venv? Escribe SI en caso de tenerlo, cualquier otro input no creara el entorno"
+if ($flag -eq "SI"){
+    Write-Host "Creando venv..."
+    python -m venv ./venv
+}
+
+
+.\venv\Scripts\Activate.ps1
+pip install -r requirements.txt
+
