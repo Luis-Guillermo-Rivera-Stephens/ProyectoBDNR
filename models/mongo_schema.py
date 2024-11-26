482b0e0e8a33a9ef672dbe1c17b9d64f447ca71e
Nicolas Navarro Valenzuela <nicolas.navarro@iteso.mx>
2024-11-25 18:46:31 -0600

    modelo de mongo

---
+++ models/mongo_schema.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+import uuid
+
+class Game(BaseModel):
+    gameID: str = Field(..., alias="_gameID")  
+    played_counter: int
+    time_playing: int
+    category: str
+
+class Category(BaseModel):
+    category: str
+    time_playing: int
+
+class UserInfo(BaseModel):
+    userID: uuid.UUID = Field(default_factory=uuid.uuid4, alias="_UserID")
+    TimePlaying: int
+    Games: List[Game] = []
+    Categories: List[Category] = []
