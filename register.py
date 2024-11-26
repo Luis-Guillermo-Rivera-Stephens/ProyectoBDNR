268cbf9d57849d6b04fe184a887193dfaff0b18b
Luis-Guillermo-Rivera-Stephens <148508811+Luis-Guillermo-Rivera-Stephens@users.noreply.github.com>
2024-11-25 19:28:11 -0600

    register change

--- register.py
+++ register.py
@@ -2,7 +2,12 @@ import login
 from queries.cassandra_queries import CASSANDRA_REGISTER_QUERY
 import uuid
 from datetime import datetime, timezone
+from models import mongo_schema
+import connections
 
+def mongo_creation(session ,user_id):
+    newuser = mongo_schema.UsersInfo(user_id, 0, [], [])
+    session.database[connections.MONGODB_COLLECTION_NAME].insert_one(newuser.dict(by_alias=True))
 
 def register(session,username, password):
     try: 
@@ -11,6 +16,8 @@ def register(session,username, password):
 
         query = session.prepare(CASSANDRA_REGISTER_QUERY)
         session.execute(query, [account_id, username, password, creation_date])
+        
+
     except Exception as e:
         print(f"Error registering user: {e}")
         return False
