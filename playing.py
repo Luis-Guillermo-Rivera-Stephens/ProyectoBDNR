e4a9df4212f8dbd52b1c11435212f07e79289b2b
Luis-Guillermo-Rivera-Stephens <148508811+Luis-Guillermo-Rivera-Stephens@users.noreply.github.com>
2024-11-26 18:29:58 -0600

    playing

---
+++ playing.py
@@ -0,0 +1,13 @@
+import datetime
+from queries import mongo_queries
+
+def playing(mongo_session, id_account, game_id, category):
+    start_time = datetime.datetime.now()
+    print("Jugando ...")
+    input("Presiona Enter para terminar")
+    end_time = datetime.datetime.now()
+
+    elapsed_time = round((end_time - start_time).total_seconds())
+    print("Tiempo de juego en segundos:", elapsed_time)
+    if elapsed_time > 0:
+        mongo_queries.update_stats(mongo_session, id_account,game_id, category, elapsed_time)
